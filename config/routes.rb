25d9e183bf0995a68d8078c5149fe74c6532a0eb
Frederic Kaveh <heydarkv@yahoo.fr>
2018-08-09 14:59:21 +0200

    controller cocktail ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/edit'
+  get 'cocktails/update'
+  get 'cocktails/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :cocktails, only: [:index, :show]
 end
